7d521b1d27d56746114e6ce1bdef293398e2c7ae
Selvaraj T <124679345+Selva-rajT@users.noreply.github.com>
2023-02-08 10:04:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function fb(){
+    window.open('https://www.facebook.com/pardeep.selva.5/', '_blank');
+}
+function li(){
+    window.open("https://www.linkedin.com/in/selva-raj-001322241/",'_blank');
+}
+function gh(){
+    window.open('https://github.com/Selva-rajT','_blank');
+}
+function gh(){
+    window.open('https://github.com/Selva-rajT','_blank');
+}
+
+function ig(){
+    window.open('https://www.instagram.com/kabaddi_lover.selva/?igshid=Mzc0YWU1OWY%3D','_blank');
+}
+function gm(){
+   window.location.href="mailto:selvaraj4029543@gmail.com";
+}
+function resume(){
+    window.open("https://www.canva.com/design/DAFMdr15OKs/M8jDJmWRLnfwUSZdXta1xw/view?utm_content=DAFMdr15OKs&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink",'-blank');
+}
